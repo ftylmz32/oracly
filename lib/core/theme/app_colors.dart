29b866f8ad12341f6b66f04e84684120087ffde0
Fatih <ftylmz32@gmail.com>
2026-07-27 11:25:35 +0300

    v0.11.4 Oracly visual color system

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,19 +1,67 @@
 import 'package:flutter/material.dart';
 
+
 class AppColors {
+
   AppColors._();
 
-  static const Color background = Color(0xFF0B0B0F);
 
-  static const Color surface = Color(0xFF17171D);
 
-  static const Color gold = Color(0xFFD4AF37);
+  // 🌌 Ana arka plan
 
-  static const Color goldLight = Color(0xFFF4D675);
+  static const Color background =
+      Color(0xFF080812);
 
-  static const Color white = Colors.white;
 
-  static const Color textSecondary = Color(0xFF9E9E9E);
 
-  static const Color border = Color(0x33D4AF37);
+  // Kart / yüzey rengi
+
+  static const Color surface =
+      Color(0xFF13131F);
+
+
+
+  // Oracly ana rengi
+
+  static const Color primary =
+      Color(0xFF8B5CF6);
+
+
+
+  // Parlak vurgu
+
+  static const Color primaryLight =
+      Color(0xFFC084FC);
+
+
+
+  // Mistik altın
+
+  static const Color gold =
+      Color(0xFFD4AF37);
+
+
+
+  static const Color goldLight =
+      Color(0xFFF4D675);
+
+
+
+  // Yazılar
+
+  static const Color white =
+      Colors.white;
+
+
+
+  static const Color textSecondary =
+      Color(0xFFA1A1AA);
+
+
+
+  // Kenarlık
+
+  static const Color border =
+      Color(0x338B5CF6);
+
 }
