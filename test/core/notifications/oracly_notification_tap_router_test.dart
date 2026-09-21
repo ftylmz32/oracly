984821b5d25e27deaa38cd221c3eb08d3e476383
ftylmz32 <ftylmz32@gmail.com>
2026-09-21 20:22:56 +0300

    Regress notification queue before navigator context readiness

--- test/core/notifications/oracly_notification_tap_router_test.dart
+++ test/core/notifications/oracly_notification_tap_router_test.dart
@@ -8,6 +8,8 @@ import 'package:oracly_new/core/notifications/oracly_notification_kind.dart';
 import 'package:oracly_new/core/notifications/oracly_notification_tap_inbox.dart';
 import 'package:oracly_new/core/notifications/oracly_notification_tap_router.dart';
 import 'package:oracly_new/shared/navigation/oracly_navigation_scope.dart';
+import 'package:oracly_new/shared/navigation/oracly_shell_bridge.dart';
+import 'package:oracly_new/shared/navigation/oracly_navigation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../test_helpers/provider_scope_harness.dart';
@@ -41,6 +43,24 @@ void main() {
       OraclyNotificationKind.discovery,
     );
   });
+
+  test(
+    'active shell without navigator context does not consume pending payload',
+    () {
+      OraclyNotificationTapInbox.instance.offer('daily');
+
+      void switcher(OraclyTab _) {}
+      OraclyShellBridge.bind(switcher);
+      addTearDown(() => OraclyShellBridge.unbind(switcher));
+
+      OraclyNotificationTapRouter.openPending();
+
+      expect(
+        OraclyNotificationTapInbox.instance.pendingKind,
+        OraclyNotificationKind.daily,
+      );
+    },
+  );
 
   testWidgets('navigator ready consumes pending payload once', (tester) async {
     OraclyNotificationTapInbox.instance.offer('companion');
